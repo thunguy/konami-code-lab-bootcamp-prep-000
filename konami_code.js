d8225668327ea88a0ec2867c059ce112a3003164
thunguy@me.com <thunguy@me.com>
2018-06-07 01:13:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,21 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let i = 0
+  
+  document.body.addEventListener('keydown', (event) => {
+    const key = e.key;
+  
+    if (key === code[i]) {
+      i++;
+      
+    if (i === code.length) {
+      alert("Hurray!");
+      i = 0;
+    }
+    else {
+      i = 0;
+    }
+   }
+ })
 }
